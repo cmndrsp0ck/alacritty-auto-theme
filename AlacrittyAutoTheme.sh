65be08c1b360c128744c858dcf28c3330eaffebc
Fabian Barajas <fbarajas@nvidia.com>
2025-06-08 21:25:18 -0700

    chore: switch theme using git instead of echo
    This commit changes the method of switching themes from using echo to
    using git. This allows for easier management of themes and prevents
    potential issues with file overwriting.

--- AlacrittyAutoTheme.sh
+++ AlacrittyAutoTheme.sh
@@ -13,10 +13,12 @@ dbus-monitor --profile "interface='$interface',path=$monitor_path,member=$monito
 			theme="$(gsettings get org.gnome.desktop.interface color-scheme)"
 			if [[ "$theme" == "'prefer-dark'" ]]; then
 				#Need to set with full paths, goofy things are happening otherwise
-				echo "$(echo import = [ \'~/.config/alacritty/alacritty-auto-theme/dark_theme.toml\' ] > ~/.config/alacritty/alacritty-auto-theme/theme.toml)"
+				# echo "$(echo import = [ \'~/.config/alacritty/alacritty-auto-theme/dark_theme.toml\' ] > ~/.config/alacritty/alacritty-auto-theme/theme.toml)"
+				git -C $HOME/.config/alacritty/ switch dark-theme
 			else
-				echo "$(echo import = [ \'~/.config/alacritty/alacritty-auto-theme/light_theme.toml\' ] > ~/.config/alacritty/alacritty-auto-theme/theme.toml)"
+				# echo "$(echo import = [ \'~/.config/alacritty/alacritty-auto-theme/light_theme.toml\' ] > ~/.config/alacritty/alacritty-auto-theme/theme.toml)"
+				git -C $HOME/.config/alacritty/ switch light-theme
 			fi
 			count=0
-		fi 
+		fi
 	done
